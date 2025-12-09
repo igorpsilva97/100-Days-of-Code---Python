0dc67ac592127f5b6a153abf90b3ae6c71c1a999
Igor Pereira da Silva <igorpesilva97@gmail.com>
2025-12-09 17:18:19 -0300

    Day 01 complete

---
+++ Day 01/main.py
@@ -0,0 +1,4 @@
+print("Welcome to band name Generator")
+pet_name = input("What is your pet name? ")
+grew_city = input("What's the name of the city you grew up in? ")
+print("Your band name could be " + grew_city + " " + pet_name)
